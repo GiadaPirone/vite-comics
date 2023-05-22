3037e20e28fc7b3b27c34ca725b912f948972e22
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-05-22 16:50:30 +0200

    img inserite

--- src/App.vue
+++ src/App.vue
@@ -13,15 +13,34 @@ export default {
     AppMenu,
     AppFooterOne,
     AppFooter,
-    data(){
+    
+  },
+  data(){
       return{
-        slides:[
-        "Action Comics",
-        "American Vampire 1976",
+        slides: [
+          {
+            thumb: "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+            price: "$19.99",
+            series: "Action Comics",
+            type: "comic book",
+          },
+
+          {
+            thumb: "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+            price: "$3.99",
+            series: "American Vampire 1976",
+            type: "comic book",
+          },
+
+          {
+            thumb: "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+            price: "$16.99",
+            series: "Aquaman",
+            type: "graphic novel",
+          }
         ]
       }
     }
-  }
 }
 
 </script>
@@ -32,7 +51,9 @@ export default {
   </header>
 
   <main>
-    <AppContent :slide="slides" />
+    <AppContent v-for="slide in slides"
+    :img="slide.thumb"
+    :bigText="slide.series" />
 
     <AppMenu />
   </main>
--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,36 +1,20 @@
 <script>
     export default{
         name: "AppContent",
-        props:[
-            "slide"
-        ]
-            
-    
-        // data(){
-        //     return{
-        //         slides: [
-        //             {
-        //                 thumb: "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
-        //                 series: "Action Comics",
-        //             },
-
-        //             {
-        //                 thumb: "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
-        //                 series: "American Vampire 1976",
-        //             },
-        //         ]
-        //     }
-        // }
-
+        props:{
+            img: String,
+            bigText:String,
+        }
     }
 </script>
 
 <template>
-    <div class="mainContent dFlex alignCenter justifyContentCenter">
-        <h1> Content goes here </h1>
-    </div>
-    <div v-for="testo in slide">
-        <div >{{ testo }}</div>
+    <div class="dFlex">
+
+        <div class="conteinerImg"> 
+            <img :src="img" alt="">
+            <p>{{ bigText }}</p>
+        </div>
     </div>
     
 
@@ -48,4 +32,11 @@
         color: white;
         
     }
+    /* .conteinerIm{
+        width: 50px;
+    } */
+    img{
+        width: 100px;
+    }
+
 </style>
